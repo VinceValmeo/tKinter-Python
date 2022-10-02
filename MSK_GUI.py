84d1e8d1ec426d3cb4134e7c94fc95d117a56fd7
JJMR2 <jonfrancis.mirto@gmail.com>
2022-10-03 00:21:55 +0800

    Jon was here

--- MSK_GUI.py
+++ MSK_GUI.py
@@ -40,19 +40,20 @@ class Example(Frame):
         procedure_label = Label(frame1, text="Classification Procedure",
                                 width=11, font=("Arial", 11))
         procedure_label.pack(fill=X, padx=5, pady=5, expand=True)
+
 # Model Selection
         lbl1 = Label(frame1, text="Choose Model",
-                     width=11, font=("Arial Bold", 12))
+                     width=11, font=("Arial Bold", 12), highlightbackground="black", highlightthickness=1)
         lbl1.pack(side=LEFT, padx=5, pady=4)
 
 
 # Classification Button
 
         def clasify_input():
-            final_classification_label.configure(
-                text="MSK is Normal", font=("Arial Bold", 11), foreground="Green")
-        classify_btn = tk.Button(frame1, text='Classify Image',
-                                 width=20, command=lambda: clasify_input())
+            classification_result.configure(text="MSK is Normal", font=("Arial Bold", 11), foreground="Green", width=20)
+            probability_result.configure(text="Probability is 30%", font=("Arial Bold", 11), width=20)
+
+        classify_btn = tk.Button(frame1, text='Classify Image', width=20, command=lambda: clasify_input())
         classify_btn.pack(side=RIGHT, padx=5, pady=4)
 
 # Classification Label
@@ -87,24 +88,40 @@ class Example(Frame):
         lbl3 = Label(frame3, text="File Upload",
                      width=11, font=("Arial Bold", 12))
         lbl3.pack(side=LEFT, padx=5, pady=4)
-# Classification Results
-        final_classification_label = Label(frame3, text="Classification Result ...",
-                                           width=20, font=("Arial Bold", 12))
-        final_classification_label.pack(side=RIGHT, padx=10, pady=4)
 
 # File Button
-        frame4 = Frame(self)
-        frame4.pack(fill=X)
+
+        # Frame Main
+        frame_main = Frame(self,bg="black", pady=30, padx=30)
+        frame_main.pack(fill=X)
+
+        # Classification Results
+        frame_results = Frame(frame_main, bg="black", pady=10, padx=10)
+        frame_results.grid(row=1, column=2)
+
+        classification_result = Label(frame_results, text="Classification Result ...", font=("Arial Bold", 12), width=18)
+        classification_result.grid(row=1,  padx=10, pady=100)
+
+        probability_result = Label(frame_results, text="Probability is...", font=("Arial Bold", 12), width=18)
+        probability_result.grid(row=2, padx=10, pady=100)
+ 
+        # Frame for Preview Image
+        frame_preview = Frame(frame_main, bg="black", width=500, height=500, highlightbackground="white", highlightthickness=1, pady=100, padx=100)
+        frame_preview.grid(row=1, column=1)
+ 
+        # Frame for Cam Image
+        frame_cam = Frame(frame_main, bg="black", width=500, height=500, highlightbackground="white", highlightthickness=1, pady=100, padx=100)
+        frame_cam.grid(row=1, column=3)
 
         def upload_file():
             global img
             f_types = [('Png Files', '*.png')]
             filename = filedialog.askopenfilename(filetypes=f_types)
             img = Image.open(filename)
-            img_resized = img.resize((500, 500))  # new width & height
+            img_resized = img.resize((300, 300))  # new width & height
             img = ImageTk.PhotoImage(img_resized)
-            uploaded_img = tk.Button(frame4, image=img)  # using Button
-            uploaded_img.pack(side=LEFT, padx=5, pady=4)
+            uploaded_img = tk.Button(frame_preview, image=img)  # using Button
+            uploaded_img.pack()
 
         def restart_program():
             python = sys.executable
@@ -114,13 +131,12 @@ class Example(Frame):
                                width=20, command=lambda: upload_file())
         upload_btn.pack(side=LEFT, padx=5, pady=4)
 
-        Button(frame3, text="Restart", command=restart_program).pack()
-
+        Button(frame3, text="Restart", command=restart_program).pack(side=RIGHT, padx=5)
 
 def main():
 
     root = Tk()
-    root.geometry("1200x600")
+    root.geometry("1280x720")
     app = Example()
     root.mainloop()
 
